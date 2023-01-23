d986281ca9a4eaacc5404078e914d185b7b66b9e
Katharina Simma <katharina.simma@diebin.at>
2023-01-23 15:45:28 +0100

    Style header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+/** @jsxImportSource @emotion/react */
+
+import { css } from '@emotion/react';
+
+const headerStyles = css`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 1.25rem 2rem;
+`;
+
+const headerItemStyle = css`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #374151;
+  font-size: 1.1rem;
+  font-weight: 500;
+
+  img {
+    margin-right: 3.4rem;
+  }
+  nav {
+    padding: 0 16px;
+  }
+
+  button {
+    color: #2563eb;
+    background-color: #dbeafe;
+    border: none;
+    padding: 0.75rem 1.25rem;
+    margin-left: 2rem;
+    font-size: 1.1rem;
+    font-weight: 500;
+    border-radius: 0.75rem;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header css={headerStyles}>
+        <div css={headerItemStyle}>
+          <a>
+            <img
+              src="https://landingpage.fyi/images/logo-fyi.svg"
+              alt="landing page fyi logo"
+            />
+          </a>
+          <nav>Landing Page Tools</nav>
+          <nav>Landing Page Checklist</nav>
+          <nav>Blog</nav>
+        </div>
+        <div css={headerItemStyle}>
+          <nav>Submit</nav>
+          <button>Subscribe</button>
+        </div>
       </header>
-    </div>
+
+      <main style={{ minHeight: '200px', border: '2px solid purple' }}>
+        Main
+      </main>
+      <footer style={{ minHeight: '200px', border: '2px solid purple' }}>
+        Footer
+      </footer>
+    </>
   );
 }
 
